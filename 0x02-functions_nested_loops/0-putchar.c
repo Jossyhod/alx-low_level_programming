1ba7a9fd6ca7a947d54cab823c5e2c4b349826ec
Joseph <ikeanyi_joseph@yahoo.com>
2023-02-21 03:45:48 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Program to print putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int str[] = {42, 78, 108, 98, 78, 56, 84, 70, 100};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
