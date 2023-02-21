6f3c43ad0ab2e7d629eddad16ee64268ead68b9c
Joseph <ikeanyi_joseph@yahoo.com>
2023-02-21 12:48:57 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Program for alphabets
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letter  'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	return (0);
+}
