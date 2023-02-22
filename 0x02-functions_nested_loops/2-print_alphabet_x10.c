7ddc7a8bdd4971cd4e6d2f7b557bccc9cffa920b
Joseph <ikeanyi_joseph@yahoo.com>
2023-02-22 11:52:29 -0800

    Lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,12 @@
 
 
 	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase 
 	 * followed by a new line
 	 */
 	void print_alphabet_x10(void)
-	{
-		char ch;
+	{ char ch;
 		int i;
-
 
 		i = 0;
 
