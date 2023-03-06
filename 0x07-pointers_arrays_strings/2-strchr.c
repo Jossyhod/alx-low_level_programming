010770bc367d058a60f60e6c2d0d5caa4f2b4eea
Joseph <ikeanyi_joseph@yahoo.com>
2023-03-06 06:22:59 -0800

    Locate String

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,17 +3,15 @@
 /** _strchr - locates a character in a string.
  * @s: string.
  * @c: character.
- * return: the pointer to the first occurence of the character c.
+ * Return: Always 0 (Success)
  */
-
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
-
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+        int i = 0;
+        for (; s[i] >= '\0'; i++)
+        {
+                if (s[i] == c)
+                        return (&s[i]);
+        }
+        return (0);
 }
